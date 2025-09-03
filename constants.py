3d550fb6bb8757e1a9998567e5290ba45e3ea3ad
Dipak Mali <=>
2025-09-03 15:40:52 +0530

    Added: Dockerfile

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 BASE_DIR = os.path.dirname(__file__)
-DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, "db", os.getenv("DEV_DATABASE_URL"))}"
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'db', os.getenv('DEV_DATABASE_URL'))}"
 LOG_FILE_PATH = os.path.join(BASE_DIR, "log", "app.log")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
